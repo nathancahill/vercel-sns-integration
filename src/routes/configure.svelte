e3d470d2772cf2da77629cf0b3d377b7b8567c8f
Nathan Cahill <nathan@nathancahill.com>
2022-05-23 08:14:15 -0400

    fix saved changes

--- src/routes/configure.svelte
+++ src/routes/configure.svelte
@@ -296,16 +296,6 @@
 				{/each}
 			</select>
 		</div>
-		<div class="flex items-end">
-			{#if unsaved && !loading}
-				<button
-					on:click={handleSave}
-					type="button"
-					class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					>Save Changes</button
-				>
-			{/if}
-		</div>
 	</div>
 
 	{#if loading}
@@ -581,6 +571,40 @@
 								<Code code={origin} language="javascript" />
 							</div>
 						{/if}
+					</div>
+				</div>
+			</div>
+		{/if}
+
+		{#if unsaved && !loading}
+			<div class="fixed mr-4 mt-4 top-0 right-0 w-96 rounded-md bg-yellow-50 p-4 shadow">
+				<div class="flex">
+					<div class="flex-shrink-0 flex items-center">
+						<!-- Heroicon name: solid/exclamation -->
+						<svg
+							class="h-5 w-5 text-yellow-400"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+					<div class="ml-3 flex-1 flex items-center">
+						<h3 class="text-sm font-medium text-yellow-800">Unsaved changes</h3>
+					</div>
+					<div>
+						<button
+							on:click={handleSave}
+							type="button"
+							class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+							>Save Changes</button
+						>
 					</div>
 				</div>
 			</div>
